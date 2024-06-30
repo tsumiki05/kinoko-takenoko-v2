034a61bc6a6603d4658185abeb1882039bf25d4c
tsumiki05 <s1F102402877@iniad.org>
2024-06-30 13:36:06 +0900

    aaa

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import re
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
+kinoko_count = 3
+takenoko_count = 5
+messages = ['kinoko is wounderful!', 'takenoko is awesome!']
 
 @app.route('/')
 def top():
@@ -7,6 +12,30 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    global kinoko_count, takenoko_count, messages
+    if request.form.get("item") == 'kinoko':
+        kinoko_count += 1
+    elif request.form.get("item") == 'takenoko':
+        takenoko_count += 1
+
+    messages.append(request.form.get("message"))
+    if len(messages) > 3:
+        messages = messages[-3:]
+
+    kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
+    takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
+
+    message_html = ''
+    for i in range(len(messages)):
+        message = messages[i]
+        message = re.sub(r'&', r'&amp;', message)
+        message = re.sub(r'<', r'&lt;', message)
+        message = re.sub(r'>', r'&gt;', message)
+        message = re.sub(r'\*(.+)\*', r'<strong>\1</strong>', message)
+        message = re.sub(r'(\d{2,3})-\d+-\d+', r'\1-****-****', message)
+        message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
+            message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
+        
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
